5a4e58b0cb2982ee18cd2bb370073039fa2aa546
Marcelo Santana <marcelo.scientist@hotmail.com>
2020-07-09 11:48:57 -0300

    Adding images as well

--- pokewindow/src/components/pokemon/PokemonCard.js
+++ pokewindow/src/components/pokemon/PokemonCard.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+`
 
 export default class PokemonCard extends Component {
+    state = {
+        name: '',
+        imageUrl : '',
+        pokemonIndex: ''
+    } 
+    componentDidMount() {
+        const { name, url } = this.props
+        const pokemonIndex = url.split('/')[url.split('/').length - 2]
+        const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+
+        this.setState({
+            name,
+            imageUrl,
+            pokemonIndex
+        })
+    }
+    
     render() {
+
+
         return (
             <div className="col-md-4 col-sm-6 mb-5">
                 <div className="card">
-                    <div className="card-header">
-                        <h1>Marcelo</h1>
-                    </div>    
-                    <div className="card-body">
-                        <button className="btn btn-success">ADD IN YOUR CART</button>
+                    <h5 className="card-header" >
+                        {this.state.pokemonIndex}
+                    </h5>
+                    <Sprite className="card-img-top rounded mx-auto mt-2"
+                        src={this.state.imageUrl}
+                    />
+
+                    
+                    <div className="card-body mx-auto">
+                        <h6 className="card-title">
+                            {this.state.name
+                                .toLowerCase()
+                                .split(' ')
+                                .map(
+                                    letter => 
+                                    letter.charAt(0).toUpperCase() + letter.substring(1)
+                                )
+                                .join(' ')
+                            }
+                        </h6>
+                        <button className="btn btn-success">ADD TO YOUR CART</button>
                     </div>      
                 </div>               
             </div>
